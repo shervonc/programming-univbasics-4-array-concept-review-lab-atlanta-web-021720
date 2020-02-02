1abd0b3c579ee75fc6cc965cc3a68a452d6cc5f2
Shervon Ceromain <shervonc@gmail.com>
2020-02-02 14:29:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
 
-array.index(value_to_find)
+array.index(value_to_find) |value_to_find|
+puts array.index(value_to_find)
 end
 
 def find_max_value(array)
