36f42c11c08f12283b456f4f1e3204a70b243238
Shervon Ceromain <shervonc@gmail.com>
2020-02-02 14:20:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,10 +5,11 @@ end
 def find_max_value(array)
   array.sort!
     puts array.last
-   
-end
+    array.last
+   end
 
 def find_min_value(array)
   array.sort!
   puts array.first
+  array.first
  end
