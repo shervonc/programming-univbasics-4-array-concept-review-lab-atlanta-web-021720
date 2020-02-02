9c0bc65081c49167fa2ab264bffeba47cdaea87f
Shervon Ceromain <shervonc@gmail.com>
2020-02-02 16:21:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -56,6 +56,7 @@ end
 # end
 def find_min_value(array)
   x = array[0]
-  array.length.times { |index| if array[index] < x   x = array[index] end}
-  x
+  array.length.times { |index| if array[index] < x   x = array[index] }
+ end
+ x
 end
