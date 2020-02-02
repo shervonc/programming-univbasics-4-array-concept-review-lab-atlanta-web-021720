b0aff4469ba3042a7c196f83b1688a18417b0201
Shervon Ceromain <shervonc@gmail.com>
2020-02-02 14:47:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,8 +7,8 @@ def find_element_index(array, value_to_find) # method 2
  count = 0
  while count < array.length do
  array[count] == value_to_find
-count += 1
 return count
+count += 1
 end
 nil
 end
