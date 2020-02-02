b10a060507f4aeb060fd2c95345a6aade30f41e6
Shervon Ceromain <shervonc@gmail.com>
2020-02-02 16:32:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find) # method 1 use block paramter
 array.index(value_to_find) {|value_to_find|
-puts array.index(value_to_find)
+return array.index(value_to_find)
 }
 end
 
